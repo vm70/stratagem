e8d7d4570f492546b46a6ea0469cbebdf48e5d9f
Vincent Mercator <vmercator@protonmail.com>
2024-10-03 19:54:47 -0400

    Remove the colon in HUD level section

--- src/ui.lua
+++ src/ui.lua
@@ -264,7 +264,7 @@ function DrawHUD(player)
 	-- the `chr(3) .. f` statement moves the text back one pixel
 	print("score: " .. chr(3) .. "f" .. LeftPad(tostr(player.score), " ", 5), 17, 9, 7)
 	print("chances: " .. chr(3) .. "f" .. max(player.chances, 0), 73, 9, 8)
-	print("level:" .. player.level, 49, 121, 7)
+	print("level " .. player.level, 49, 121, 7)
 	-- calculate level completion ratio
 	local level_ratio = (player.score - player.init_level_score) / (player.level_threshold - player.init_level_score)
 	level_ratio = min(level_ratio, 1)
